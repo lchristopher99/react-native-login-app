dcb55f6715bb4cd80dfcfb87eacad88b679f9378
logan <logan.christopher@comcast.net>
2018-06-25 15:29:13 -0500

    Finished login logic

--- App.js
+++ App.js
@@ -1,63 +1,46 @@
-import React, {Component} from 'react';
-import {StyleSheet, ScrollView, Image} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, ScrollView, Image } from 'react-native';
 
 import SubmitCredsBtn from './components/login/submitCreds';
-import UserCredsState, {getUserCreds} from './components/login/userInput';
-import {UserInput} from './components/login/userInput1';
+import InputVerification from './components/login/inputVerification';
+//import welcomeScreen from './screens/welcomeScreen';
+
 // import LoginErrModal from './components/login/loginErrModal';
-import CredsAPI from './components/API/credsAPI';
+// import CredsAPI from './components/API/credsAPI';
 
 export default class App extends Component {
-  onGetUserCreds() {
-    this.credsAPI.credsApiFunction();
-  }
-
-  // onModalActivated() { // This local function calls the function within LoginErrModal to toggle visible
-  //   this.errorModal.setModalVisible(true); // this handler is identified by the ref errorModal set within the component
-  // }
-
-  onGetUserCredsState() {
-    this.userCredsState.getUserCreds();
-  }
-
   render() {
+    //const { navigate } = this.props.navigation;
     return (
-      <ScrollView 
-				scrollEnabled={false}
-				keyboardShouldPersistTaps='handled'
-				keyboardDismissMode='on-drag'
-				contentContainerStyle={styles.container}>
-        <Image 
-          resizeMode='center' source={require('./images/lojixLogo.png')} 
+      <ScrollView
+        scrollEnabled={false}
+        keyboardShouldPersistTaps='handled'
+        keyboardDismissMode='on-drag'
+        contentContainerStyle={styles.container}>
+        <Image
+          resizeMode='center' source={require('./images/lojixLogo.png')}
         />
-        <UserInput/>
-        <UserCredsState
-          ref={ref => (this.userCredsState = ref)}
+        <InputVerification
+          ref={ref => (this.inputVerification = ref)}
         />
         <SubmitCredsBtn
-          //onGetCreds={this.onModalActivated.bind(this)} // error modal uses this handler
-          //onGetCreds={this.onGetUserCreds.bind(this)} // credsAPI uses this handler
-          //onGetCreds={getUserCreds} // this is the exported arrow function from userInput.js
-          onGetCreds={this.onGetUserCredsState.bind(this)}
-        />
-        {/* <LoginErrModal // fix this modal to appear for 404 and form errors
-          ref={ref => (this.errorModal = ref)} // This component is an exported class from loginErrModal
-        />  */}
-        <CredsAPI
-          ref={ref => (this.credsAPI = ref)}
+          //onGetCreds={() => this.errorModal.setModalVisible(true)} // error modal uses this handler
+          //onGetCreds={() => this.credsAPI.credsApiFunction()} // credsAPI uses this handler
+          onGetCreds={() => this.inputVerification.getUserCreds()}
         />
       </ScrollView>
     );
   }
 }
 
+
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#fff',
     alignItems: 'center',
-		justifyContent: 'center',
-		flexDirection: 'column',
-		flex: 1,
-		bottom: 50
-	}
+    justifyContent: 'center',
+    flexDirection: 'column',
+    flex: 1,
+    bottom: 80
+  }
 });
---
+++ components/login/userInputForm.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+
+import t from 'tcomb-form-native';
+
+export default class UserInputForm extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Form
+          ref={a => this.submittedForm = a}
+          options={options}
+          type={User} 
+        />
+        <Form
+          ref={b => this.submittedHost = b}
+          options={options}
+          type={Host}
+        />
+      </View>
+    );
+  }
+}
+
+const Form = t.form.Form;
+
+const User = t.struct({
+  username: t.String,
+  password: t.String,
+});
+
+const Host = t.struct({
+  hostname: t.String
+});
+
+const options = {
+  fields: {
+    password: {
+      password: true,
+      secureTextEntry: true
+    },
+    hostname: {
+      autoCapitalize: 'none'
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '70%'
+  },
+});
